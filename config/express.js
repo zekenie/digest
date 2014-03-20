89ba553c2f3ccb9492b3774fd7e89e648a2b5da2
Zeke Nierenberg <znierenberg@fiksu.com>
2014-03-20 12:34:00 -0400

    making modifications for express 4

--- config/express.js
+++ config/express.js
@@ -7,6 +7,9 @@ var express = require('express')
   , mongoStore = require('connect-mongo')(express)
   , flash = require('connect-flash')
   , pkg = require('../package.json')
+  , bodyParser = require('body-parser')
+  , cookieParser = require('cookie-parser')
+  , methodOverride = require('method-override')
 
 var env = process.env.NODE_ENV || 'development'
 
@@ -28,81 +31,76 @@ module.exports = function (app, config, passport) {
   app.set('views', config.root + '/views')
   app.set('view engine', 'ejs')
 
-  app.configure(function () {
-    // expose package.json to views
-    app.use(function (req, res, next) {
-      res.locals.pkg = pkg
+  // expose package.json to views
+  app.use(function (req, res, next) {
+    res.locals.pkg = pkg
+    next()
+  })
+
+  // cookieParser should be above session
+  app.use(cookieParser())
+
+  // bodyParser should be above methodOverride
+  app.use(bodyParser())
+  app.use(methodOverride())
+
+  // express/mongo session storage
+  app.use(express.session({
+    secret: 'foo',
+    store: new mongoStore({
+      url: config.mongo,
+      db : 'sessions'
+    })
+  }))
+
+  // use passport session
+  app.use(passport.initialize())
+  app.use(passport.session())
+
+  // connect flash for flash messages - should be declared after sessions
+  app.use(flash())
+
+  // adds CSRF support
+  if (process.env.NODE_ENV !== 'test') {
+    app.use(express.csrf())
+
+    // This could be moved to view-helpers :-)
+    app.use(function(req, res, next){
+      res.locals.csrf_token = req.csrfToken()
       next()
     })
+  }
 
-    // cookieParser should be above session
-    app.use(express.cookieParser())
 
-    // bodyParser should be above methodOverride
-    app.use(express.bodyParser())
-    app.use(express.methodOverride())
-
-    // express/mongo session storage
-    app.use(express.session({
-      secret: 'foo',
-      store: new mongoStore({
-        url: config.mongo,
-        db : 'sessions'
-      })
-    }))
-
-    // use passport session
-    app.use(passport.initialize())
-    app.use(passport.session())
-
-    // connect flash for flash messages - should be declared after sessions
-    app.use(flash())
-
-    // adds CSRF support
-    if (process.env.NODE_ENV !== 'test') {
-      app.use(express.csrf())
-
-      // This could be moved to view-helpers :-)
-      app.use(function(req, res, next){
-        res.locals.csrf_token = req.csrfToken()
-        next()
-      })
+  // assume "not found" in the error msgs
+  // is a 404. this is somewhat silly, but
+  // valid, you can do whatever you like, set
+  // properties, use instanceof etc.
+  app.use(function(err, req, res, next){
+    // treat as 404
+    if (err.message
+      && (~err.message.indexOf('not found')
+      || (~err.message.indexOf('Cast to ObjectId failed')))) {
+      return next()
     }
 
-    // routes should be at the last
-    app.use(app.router)
+    // log it
+    // send emails if you want
+    console.error(err.stack)
 
-    // assume "not found" in the error msgs
-    // is a 404. this is somewhat silly, but
-    // valid, you can do whatever you like, set
-    // properties, use instanceof etc.
-    app.use(function(err, req, res, next){
-      // treat as 404
-      if (err.message
-        && (~err.message.indexOf('not found')
-        || (~err.message.indexOf('Cast to ObjectId failed')))) {
-        return next()
-      }
+    // error page
+    res.status(500).render('500', { error: err.stack })
+  })
 
-      // log it
-      // send emails if you want
-      console.error(err.stack)
-
-      // error page
-      res.status(500).render('500', { error: err.stack })
-    })
-
-    // assume 404 since no middleware responded
-    app.use(function(req, res, next){
-      res.status(404).render('404', {
-        url: req.originalUrl,
-        error: 'Not found'
-      })
+  // assume 404 since no middleware responded
+  app.use(function(req, res, next){
+    res.status(404).render('404', {
+      url: req.originalUrl,
+      error: 'Not found'
     })
   })
 
   // development env config
-  app.configure('development', function () {
+  if(env === 'development')
     app.locals.pretty = true
-  });
 }
